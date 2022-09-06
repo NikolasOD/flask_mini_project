cf0cf25161e8bfc08f0fb0876814014462d43e4b
Bazan Nikolay <bazan.nick.art@gmail.com>
2022-09-06 21:31:46 +0300

    Lesson 9

--- application.py
+++ application.py
@@ -2,6 +2,9 @@ import sqlite3
 from werkzeug.exceptions import abort
 from flask import Flask
 from flask import render_template
+from flask import request
+from flask import redirect
+from flask import url_for
 
 app = Flask(__name__)
 
@@ -33,6 +36,56 @@ def detail_book(book_id):
     if book is None:
         abort(404)
     return render_template('books/details.html', book=book)
+
+
+@app.route('/books/create', methods=('GET', 'POST'))
+def create_book():
+    if request.method == 'POST':
+        title = request.form['title']
+        author = request.form['author']
+
+        if title and author:
+            conn = get_connection()
+            conn.execute('INSERT INTO books (title, author) VALUES (?, ?)', (title, author))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('list_books'))
+    return render_template('books/create.html')
+
+
+@app.route('/books/update/<int:book_id>', methods=('GET', 'POST'))
+def edit_book(book_id):
+    conn = get_connection()
+    book = conn.execute('SELECT * FROM books WHERE id = ?;', (book_id,)).fetchone()
+    conn.close()
+
+    if request.method == 'POST':
+        title = request.form['title']
+        author = request.form['author']
+
+        if title and author:
+            conn = get_connection()
+            conn.execute('UPDATE books SET title = ?, author = ? WHERE id = ?', (title, author, book_id))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('list_books'))
+
+    return render_template('books/edit.html', book=book)
+
+
+@app.route('/books/delete/<int:book_id>', methods=('GET', 'POST'))
+def delete_book(book_id):
+    conn = get_connection()
+    book = conn.execute('SELECT * FROM books WHERE id = ?;', (book_id,)).fetchone()
+    conn.close()
+
+    if request.method == 'POST':
+        conn = get_connection()
+        conn.execute('DELETE FROM books WHERE id = ?', (book_id,))
+        conn.commit()
+        conn.close()
+        return redirect(url_for('list_books'))
+    return render_template('books/delete.html', book=book)
 
 
 app.run(debug=True, port=5050)
